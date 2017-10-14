0227bc476077a0f0124b898d163074b60ba686c9
Jack Treble <jacktreble93@gmail.com>
2017-10-14 12:48:35 +0100

    Sprint Boot

---
+++ src/main/java/oneandahalfbeards/fitcity/FitCityApplication.java
@@ -0,0 +1,12 @@
+package oneandahalfbeards.fitcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitCityApplication.class, args);
+	}
+}
