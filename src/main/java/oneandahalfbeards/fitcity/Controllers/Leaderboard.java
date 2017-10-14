d04d67ae5d57d97ba5c902e57c8cb5c32f8157f2
George Salter <george.salter93@gmail.com>
2017-10-14 16:15:23 +0100

    Added live leaderboard functionality

--- src/main/java/oneandahalfbeards/fitcity/Controllers/Leaderboard.java
+++ src/main/java/oneandahalfbeards/fitcity/Controllers/Leaderboard.java
@@ -2,10 +2,20 @@ package oneandahalfbeards.fitcity.Controllers;
 
 import java.util.List;
 
-public class Leaderboard{
-        private List<LeaderboardPosition> leaderboard;
+public class Leaderboard {
+
+    private List<LeaderboardPosition> leaderboard;
 
     public Leaderboard(List<LeaderboardPosition> leaderboard) {
         this.leaderboard = leaderboard;
     }
+
+    public List<LeaderboardPosition> getLeaderboard() {
+        return leaderboard;
+    }
+
+    public void setLeaderboard(List<LeaderboardPosition> leaderboard) {
+        this.leaderboard = leaderboard;
+    }
+
 }
--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,8 +1,7 @@
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+angular.module('controller', [])
+.controller('leaderboard', function($scope, $http) {
+    $http.get('http://localhost:8080/leaderBoard').
+        then(function(response) {
+            $scope.leaderboard = response.data;
+        });
+});
